f9cce2b9c56cb2978143b3322f28211343fb8cee
Tuomas Kiviniemi <tuomas.kiviniemi@gmail.com>
2023-02-28 18:25:50 +0200

    update and deleteTodo queries to front api

--- frontend/src/todos/api/todos.js
+++ frontend/src/todos/api/todos.js
@@ -19,3 +19,30 @@ export const createTodo = async ({ task, tag, done }) => {
   });
   return await res.json();
 };
+
+export const updateTodo = async ({ id, task, tag, done }) => {
+  const res = await fetch(`http://localhost:5000/api/todos/${id}`, {
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      task,
+      tag,
+      done,
+    }),
+  });
+  return await res.json();
+};
+
+export const deleteTodo = async (id) => {
+  const res = await fetch(`http://localhost:5000/api/todos/${id}`, {
+    method: 'DELETE',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+  return await res.json();
+};
