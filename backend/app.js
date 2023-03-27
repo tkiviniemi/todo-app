16c616c7c8e1877ebe17eb8bd6a8df00404be021
Tuomas Kiviniemi <tuomas.kiviniemi@gmail.com>
2023-03-27 20:14:38 +0300

    Added deployed frontend to CORS

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://todo-app-mirxii.onrender.com/'],
   })
 );
 
